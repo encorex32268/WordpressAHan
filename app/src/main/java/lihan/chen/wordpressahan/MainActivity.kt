f5ed457155c2f163dcfd97fcda3457284e9486ed
LiHan <encorex32268@gmail.com>
2023-11-20 21:58:32 +0900

    Date Picker

--- app/src/main/java/lihan/chen/wordpressahan/MainActivity.kt
+++ app/src/main/java/lihan/chen/wordpressahan/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,6 +15,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import lihan.chen.wordpressahan.feature.datepicker.DatePickerScreen
 import lihan.chen.wordpressahan.feature.expandablelist.ExpandableList
 import lihan.chen.wordpressahan.feature.expandablelist.ExpandableViewModel
 import lihan.chen.wordpressahan.feature.expandablelist.User
@@ -23,13 +25,14 @@ import lihan.chen.wordpressahan.feature.norippleclick.NoRippleClick
 import lihan.chen.wordpressahan.feature.tabhost.TabHostWordpress
 import lihan.chen.wordpressahan.ui.theme.WordpressAHanTheme
 
+@ExperimentalMaterial3Api
 @ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WordpressAHanTheme {
-                ImagePicker()
+                DatePickerScreen()
             }
         }
     }
